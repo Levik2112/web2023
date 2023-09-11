70bcd30949831cfdc96e10ddd58c499543bc2c50
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-09-11 10:59:40 +0200

    sad

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+for (int i = 10; i < 96; i++)
+{
+    if (i % 5 == 0)
+
+    {
+        Console.Write(i + " ; ");
+
+    }
+    
+
+}
+
+
+
+int second = 0;
+
+while (second < 96) 
+{
+    Console.WriteLine(second += 1);
+
+    if (second%5==0)
+    {
+        Console.Write(second + " ; ");
+    }
+
+
+}
+    
+
+    
+
+    
+    
+
+
+
+
---
+++ c#/hazi/hazi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 10; i < 96; i++)
+            {
+                if (i % 5 == 0)
+
+                {
+                    Console.Write(i + " ; ");
+
+                }
+
+
+            }
+
+
+
+            int second = 0;
+
+            while (second < 96)
+            {
+                Console.WriteLine(second += 1);
+
+                if (second % 5 == 0)
+                {
+                    Console.Write(second + " ; ");
+                }
+
+
+            }
+
+        }
+    }
+
+}
