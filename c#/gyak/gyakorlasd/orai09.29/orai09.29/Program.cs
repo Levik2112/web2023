7b663fde7eb2093f05e79d0b7d588eb94f037170
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-10-02 14:03:28 +0200

    asd

---
+++ c#/gyak/gyakorlasd/orai09.29/orai09.29/Program.cs
@@ -0,0 +1,33 @@
+﻿//addig kérjen be számot amig 5-el oszthatot nem kap majd nezze meg hogy 10 oszthato e
+
+    
+        int szam;
+
+        while (true)
+        {
+            Console.Write("Kérem adjon meg egy számot: ");
+            if (int.TryParse(Console.ReadLine(), out szam))
+            {
+                if (szam % 5 == 0)
+                {
+            Console.WriteLine("Oszthato 5-el");
+                    if (szam % 10 == 0)
+                    {
+                        Console.WriteLine("A megadott szám osztható 10-zel is.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A megadott szám nem osztható 10-zel.");
+                    }
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("A megadott szám nem osztható 5-tel.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hibás bemenet. Kérem adjon meg egy érvényes számot.");
+            }
+        }
---
+++ c#/gyak/gyakorlasd/oraigyak29/oraigyak29/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//formázott kiiratás
+
+/*
+
+Console.WriteLine("szoveg {0:0.00%} szoveg {1,10}---",12,3.4);
+
+int a = 1;
+double b = 3.4;
+Console.WriteLine($"szoveg {a} szoveg {b}");
+
+Console.WriteLine("{0,10}",1);
+Console.WriteLine("{0,10}", 10);
+Console.WriteLine("{0,10}", 100);
+Console.WriteLine("{0,10}", 1000);
+*/
+/*
+int a = 3;
+int b = 4;
+double c=Math.Sqrt(a*a + b*b);
+Console.WriteLine(c);
+for (int i = 0; i < 50; i++)
+{
+    for (int k = 0; k < 50; k++)
+    c=Math.Sqrt(i*i + k*k);
+    if (c %1== 0)
+    {
+        Console.WriteLine("{0,5 {1,5}{2,5}", i,k, c);
+    }
+}
+*/
+for (int i = 0; i <10; i++)
+{
+    Console.WriteLine("{0:0.00}",Math.Sqrt(i));
+}
