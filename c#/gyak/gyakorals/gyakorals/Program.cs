677dcc60505021ac51272dcd1ae9d5385ed581fa
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 13:17:46 +0200

    asd

---
+++ c#/gyak/gyakorals/gyakorals/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+Console.Write("Hány db szót szeretnél?: ");
+int szoSzam = Convert.ToInt32(Console.ReadLine());
+string[] szoveg = new string[szoSzam];
+for (int i = 0; i < szoveg.Length; i++)
+{
+    Console.Write("Kérek egy szót: ");
+    szoveg[i]=Console.ReadLine();
+}
+
+string max = "";
+for (int i = 0; i < szoveg.Length; i++)
+{
+    if (szoveg[i].Length > max.Length)
+    {
+        max = szoveg[i];
+    }
+}
+Console.WriteLine("A leghosszabb szó: {0}",max);
+
+//index keresés
+int maxIndex = 0;
+for (int i = 0; i < szoveg.Length; i++)
+{
+    if (szoveg[i]==max)
+    {
+        maxIndex = i;
+    }
+}
+
+//max keresés 3
+maxIndex = 0;
+for(int i = 0;i < szoveg.Length; i++)
+{
+    if (szoveg[i].Length > szoveg[maxIndex].Length) 
+    {
+    maxIndex = i;
+    }
+}
+
+
+string[] szavak = new string[] { "alma","körte",
+                                "narancs","mandula",
+                                "ananász","banán"} ;
+
+string[] rovid = new string[szavak.Length];
+string[] hosszu = new string[szavak.Length];
+
+int rovidIndex=0;
+int hosszuIndex=0;
+
+for (int i = 0;i < szavak.Length;i++)
+{
+    if (szavak[i].Length<=5)
+    {
+        rovid[rovidIndex] = szavak[i];
+        rovidIndex++;
+    }
+    else
+    {
+        hosszu[hosszuIndex++] = szavak[i];
+        
+    }
+}
+
+foreach(string i in rovid)
+{
+    Console.WriteLine(i);
+}
+foreach (string i in hosszu)
+{
+    Console.WriteLine(i);
+}
+*/
+
+//hf: oldjuk meg h ne legyyen közte hézag
+
+
+int[] szamok = new int[1000];
+
+Random rnd = new Random();
+
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = rnd.Next(100,999);
+    
+}
+foreach (int i in szamok)
+{
+    Console.Write(i+" ");   
+}
--- c#/toombok/tombok/tombok/Program.cs
+++ c#/toombok/tombok/tombok/Program.cs
@@ -1,4 +1,6 @@
-﻿void kiir(int[] tomb)
+﻿
+/*
+void kiir(int[] tomb)
 {
     foreach (int i in tomb)
     {
@@ -11,12 +13,12 @@ for (int i = 1; i < szamok.Length; i++)
 {
     szamok[i] = i+1;
 }
-/*
+
 foreach (int i in szamok)
 {
     Console.WriteLine("{0}. szám",i);
 }
-*/
+
 
 
 for (int i = 0; i < szamok.Length; i++)
@@ -25,5 +27,43 @@ for (int i = 0; i < szamok.Length; i++)
 }
 
 kiir(szamok);
+*/
 
 //hf:a szorzo tabla 1-10ig megvalositasa 2dimenzios tombbel és kiirasa táblázat szeruen
+
+Console.Write("Hány sor legyen?: ");
+int sor = Convert.ToInt32(Console.ReadLine());
+Console.Write("Hány oszlop legyen?: ");
+int oszlop = Convert.ToInt32(Console.ReadLine());
+
+string[,] tabla = new string[sor,oszlop];
+for(int i = 0; i < tabla.GetLength(0); i++)
+{
+    for(int k = 0; k < tabla.GetLength(1); k++)
+    {
+        tabla[i, k] = "";
+        for(int m = 0; m <i+1 ; m++)
+        {
+            tabla[i, k] = tabla[i,k]+"a";
+        }
+        for (int m = 0; m < k+1; m++)
+        {
+            tabla[i, k] = tabla[i,k]+"b";
+        }
+    }
+}
+/*
+foreach  (string szo in tabla)
+{
+    Console.WriteLine(szo); 
+}
+*/
+
+for (int i = 0; i < tabla.GetLength(0); i++)
+{
+    for (int k = 0; k < tabla.GetLength(1); k++)
+    {
+        Console.Write("{0} ",tabla[i, k]);
+    }
+    Console.WriteLine();
+}
