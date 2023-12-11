3ffbe2af516eb66bf03e99c86904980ddb82810e
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-12-11 10:51:04 +0100

    asd

---
+++ c#/gyak/fejviras/fejvagyiras/fejvagyiras/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> lista1 = new List<string>();
+string[] lista2 = File.ReadAllLines("kiserlet.txt");
+StreamReader olvas = new StreamReader("kiserlet.txt");
+olvas.ReadLine();
+while (!olvas.EndOfStream)
+{
+    lista1.Add(olvas.ReadLine());
+}
+olvas.Close();
+for (int i = 0; i < lista1.Count; i++)
+{
+    Console.WriteLine(lista1[i]);
+}
+
+for(int i = 0; i < lista2.Length; i++)
+{
+  //Console.WriteLine(lista2[i]);
+}
