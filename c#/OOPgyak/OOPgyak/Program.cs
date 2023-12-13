6056e0eaf7e34390b734fc69e89f2b5e656133de
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-12-13 11:56:09 +0100

    asd

---
+++ c#/OOPgyak/OOPgyak/Program.cs
@@ -0,0 +1,31 @@
+﻿class auto
+{
+    public int kerekSzam = 4;
+    public void dudal()
+    {
+        Console.WriteLine("tütütütü!");
+    }
+    private string tulaj;
+    public string tulajdonos
+    {
+        get
+        {
+            return tulaj;
+        }
+        set
+        {
+            tulaj = value;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        auto kocsi = new auto();
+        Console.WriteLine(kocsi.kerekSzam);
+        kocsi.dudal();
+        kocsi.tulajdonos = "gabui";
+        Console.WriteLine(kocsi.tulajdonos);
+    }
+}
