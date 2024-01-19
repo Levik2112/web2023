c3f1c67498601ed5e572cd5cd8a98b8eb15406de
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2024-01-19 11:28:35 +0100

    asd

---
+++ c#/helsinki1952/helsinki1952/Program.cs
@@ -0,0 +1,22 @@
+﻿using helsinki1952;
+
+List<adatok> eredmenyek = new List<adatok>();
+
+StreamReader olvas = new StreamReader("helsinki.txt");
+while (!olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    eredmenyek.Add(new adatok(sor));
+}
+olvas.Close();
+Console.WriteLine("3.Feladat");
+Console.WriteLine("Pontszerző helyezsek száma: {0}",eredmenyek.Count);
+
+int[] ermekSzama = new int[7];
+
+for (int i = 0; i < eredmenyek.Count; i++)
+{
+    ermekSzama[eredmenyek[i].helyezes]++;
+}
+Console.WriteLine("4. Feladat");
+Console.WriteLine("Arany: {0}\nEzüst: {1}\nBronz: {2}\nÖsszesen: {3}", ermekSzama[1], ermekSzama[2], ermekSzama[3], ermekSzama[1]+ermekSzama[2]+ermekSzama[3]);
--- new 1.sql
+++ new 1.sql
@@ -9,3 +9,17 @@ CREATE TABLE oktato2tantargy (id int(10) AUTO_INCREMENT PRIMARY KEY,oktatoid INT
 CREATE TABLE osztaly2agazat (id int(10) AUTO_INCREMENT PRIMARY KEY,osztalyid INT,agazatid INT);
 CREATE TABLE oszaly(id int(10) AUTO_INCREMENT PRIMARY KEY,evfolyam INT,betujel VARCHAR(10),osztalyfonokid int);
 CREATE TABLE idopont(id int(10) AUTO_INCREMENT PRIMARY KEY,datum DATE,ora int,osztalyid int,oktato2tantargyid int);
+
+CREATE TABLE eredeti(
+    id int(10) AUTO_INCREMENT PRIMARY KEY,
+    ora int(10),
+    osztaly VARCHAR(50),
+    agazat VARCHAR(50),
+    tantargy VARCHAR(50),
+    oktato VARCHAR(50),
+    ofo VARCHAR(50),
+    datum DATE
+
+);
+INSERT INTO eredeti (ora,osztaly,agazat,tantargy,oktato,ofo,datum)
+VALUES("");
